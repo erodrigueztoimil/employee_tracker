e44830bdef340ce55a7fd759cb4146e156816fab
Ernesto Rodriguez <erodrigueztoimil@gmail.com>
2020-04-19 21:23:45 -0400

    added more user features

--- README.md
+++ README.md
@@ -5,15 +5,14 @@ Author: Ernesto Rodriguez
 ## Description
 
 ### Unit 12 MySQL Homework: Employee Tracker
+
 Developers are often tasked with creating interfaces that make it easy for non-developers to view and interact with information stored in databases. Often these interfaces are known as **C**ontent **M**anagement **S**ystems. In this homework assignment, your challenge is to architect and build a solution for managing a company's employees using node, inquirer, and MySQL.
 
 ## Usage
 
-- git clone https://github.com/erodrigueztoimil/employee_tracker.git
-- cd employee_tracker
-- yarn
-- node server.js
+- `npm install` - install dependencies
+- `npm run server` - start express server
 
 ## Interface
 
-![Employee Tracker](Assets/employee-tracker.gif)
+![Employee Tracker](assets/employee-tracker.gif)
--- server.js
+++ server.js
@@ -1,44 +1,3 @@
-/*
-user options
-view all employees
-view all employees by department
-view all employees by manager
-add employee
-remove employee
-update employee role
-epdate employee manager
-*/
-
-/*
-departments
-sales
-engineering
-finance
-legal
-*/
-
-/*
-employee roles
-sales lead
-salesperson
-lead engineer
-software engineer
-account manager
-accountant
-legal team lead
-*/
-
-/*
-managers name
-John Doe
-Mike Chan
-Ashley Rodriguez
-Kevin Tupik
-Malia Brown
-Sarah Lourd
-Tom Allen
-*/
-
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const table = require("console.table");
@@ -61,59 +20,49 @@ function getUserInput() {
   inquirer
     .prompt({
       type: "list",
-      name: "action",
+      name: "choice",
       message: "What would you like to do?",
       choices: [
-        "View all employees",
-        "View all employees by department",
-        "View all employees by manager",
-        "Add employee",
-        "Remove employee",
-        "Update employee role",
-        "Update employee manager",
+        "View Employees",
+        "View Departments",
+        "View Roles",
+        "Add Employee",
+        "Add Department",
+        "Add Role",
+        "Update Employee Role",
       ],
     })
-    .then((choice) => {
-      switch (choice.action) {
-        case "View all employees":
+    .then((response) => {
+      switch (response.choice) {
+        case "View Employees":
           getEmployees();
           break;
 
-        case "View all employees by department":
-          connection.query("SELECT name FROM department", (err, response) => {
-            if (err) throw err;
-            console.log(response);
-          });
-
-          // inquirer.prompt({
-          //   type: "list",
-          //   name: "action",
-          //   choices: respon,
-          // });
+        case "View Departments":
+          getDepartments();
           break;
 
-        case "View all employees by manager":
-          console.log("option");
+        case "View Roles":
+          getRoles();
           break;
 
-        case "Add employee":
-          console.log("option");
+        case "Add Employee":
+          addEmployee();
           break;
 
-        case "Remove employee":
-          console.log("option");
+        case "Add Department":
+          addDepartment();
           break;
 
-        case "Update employee role":
-          console.log("option");
+        case "Add Role":
+          addRole();
           break;
 
-        case "Update employee manager":
-          console.log("option");
+        case "Update Employee Role":
+          updateEmployee();
           break;
 
-        case "exit":
-          connection.end();
+        default:
           break;
       }
     });
@@ -125,13 +74,165 @@ function getEmployees() {
     (err, response) => {
       if (err) throw err;
       console.table(response);
+      getUserInput();
     }
   );
 }
 
 function getDepartments() {
-  connection.query("SELECT name FROM department", (err, response) => {
+  connection.query("SELECT id, department FROM department", (err, response) => {
     if (err) throw err;
-    return response;
+    console.table(response);
+    getUserInput();
   });
+}
+
+function getRoles() {
+  connection.query("SELECT id, title FROM role", (err, response) => {
+    if (err) throw err;
+    console.table(response);
+    getUserInput();
+  });
+}
+
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        name: "first_name",
+        message: "First Name:",
+      },
+      {
+        name: "last_name",
+        message: "Last Name:",
+      },
+      {
+        name: "role_id",
+        message: "Role Id:",
+      },
+      {
+        name: "manager_id",
+        message: "Manager Id:",
+      },
+    ])
+    .then((response) => {
+      let first_name = response.first_name;
+      let last_name = response.last_name;
+      let role_id = parseInt(response.role_id);
+      let manager_id = parseInt(response.manager_id);
+
+      connection.query(
+        "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)",
+        [first_name, last_name, role_id, manager_id],
+        (err, response) => {
+          if (err) throw err;
+          console.log("Employee Added!");
+          getUserInput();
+        }
+      );
+    });
+}
+
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        name: "department",
+        message: "Department Name:",
+      },
+    ])
+    .then((response) => {
+      let department = response.department;
+
+      connection.query(
+        "INSERT INTO department (department) VALUES (?)",
+        [department],
+        (err, response) => {
+          if (err) throw err;
+          console.log("Department Added!");
+          getUserInput();
+        }
+      );
+    });
+}
+
+function addRole() {
+  inquirer
+    .prompt([
+      {
+        name: "title",
+        message: "Role Title:",
+      },
+      {
+        name: "salary",
+        message: "Salary:",
+      },
+      {
+        name: "department_id",
+        message: "Department Id:",
+      },
+    ])
+    .then((response) => {
+      let title = response.title;
+      let salary = parseInt(response.salary);
+      let department_id = parseInt(response.department_id);
+
+      connection.query(
+        "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)",
+        [title, salary, department_id],
+        (err, response) => {
+          if (err) throw err;
+          console.log("Role Added!");
+          getUserInput();
+        }
+      );
+    });
+}
+
+function updateEmployee() {
+  connection.query(
+    "SELECT employee.first_name, employee.last_name, role.title FROM employee INNER JOIN role ON employee.role_id=role.id",
+    (err, response) => {
+      const choices = [];
+
+      response.map((info) => {
+        choices.push(`${info.first_name} ${info.last_name} - ${info.title}`);
+      });
+
+      inquirer
+        .prompt([
+          {
+            type: "list",
+            name: "employee",
+            message: "What employee do you want to update?",
+            choices: choices,
+          },
+        ])
+        .then((choice) => {
+          inquirer
+            .prompt([
+              {
+                name: "new_role",
+                message: "Whats the new role id?",
+              },
+            ])
+            .then((response) => {
+              let employee = choice.employee.split(" ");
+              let first_name = employee[0];
+              let last_name = employee[1];
+              let new_role_id = parseInt(response.new_role);
+
+              connection.query(
+                "UPDATE employee SET role_id=? WHERE first_name=? && last_name=?",
+                [new_role_id, first_name, last_name],
+                (err, response) => {
+                  if (err) throw err;
+                  console.log("Employee Role Updated!");
+                  getUserInput();
+                }
+              );
+            });
+        });
+    }
+  );
 }
